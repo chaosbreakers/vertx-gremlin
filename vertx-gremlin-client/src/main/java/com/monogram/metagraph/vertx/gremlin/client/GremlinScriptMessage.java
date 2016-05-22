5ed0a648a440910cd180271b9e3a02a2873bf024
weston <weston_contribute@163.com>
2016-05-22 11:03:06 +0800

    add

--- vertx-gremlin-client/src/main/java/com/monogram/metagraph/vertx/gremlin/client/GremlinScriptMessage.java
+++ vertx-gremlin-client/src/main/java/com/monogram/metagraph/vertx/gremlin/client/GremlinScriptMessage.java
@@ -1,7 +1,37 @@
 package com.monogram.metagraph.vertx.gremlin.client;
 
+import java.util.Map;
+
 /**
  * Created by zhaoliang(weston_contribute@163.com) on 2016/5/21.
  */
 public class GremlinScriptMessage {
+
+    private String gremlinScript;
+    private Map<String, String> parambindings;
+    private String graphId;
+
+    public String getGremlinScript() {
+        return gremlinScript;
+    }
+
+    public void setGremlinScript(String gremlinScript) {
+        this.gremlinScript = gremlinScript;
+    }
+
+    public Map<String, String> getParambindings() {
+        return parambindings;
+    }
+
+    public void setParambindings(Map<String, String> parambindings) {
+        this.parambindings = parambindings;
+    }
+
+    public String getGraphId() {
+        return graphId;
+    }
+
+    public void setGraphId(String graphId) {
+        this.graphId = graphId;
+    }
 }
--- vertx-gremlin-client/src/main/java/com/monogram/metagraph/vertx/gremlin/client/VertexGremlinClient.java
+++ vertx-gremlin-client/src/main/java/com/monogram/metagraph/vertx/gremlin/client/VertexGremlinClient.java
@@ -1,7 +1,32 @@
 package com.monogram.metagraph.vertx.gremlin.client;
 
+import io.vertx.codegen.annotations.VertxGen;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.net.NetServer;
+
 /**
  * Created by zhaoliang(weston_contribute@163.com) on 2016/5/21.
  */
-public interface VertexGremlinClient {
+@VertxGen
+public class VertexGremlinClient {
+
+    private NetServer server;
+
+    public NetServer connect(Vertx vertx, JsonObject config, String host, int port) {
+        return vertx.createNetServer().listen(port, host);
+    }
+
+    public void close() {
+        server.close();
+    }
+
+    public Object execute(GremlinScriptMessage message) {
+        return null;
+    }
+
+    public Object execute(String gremlinScriptMessageJson){
+        return null;
+    }
+
 }
