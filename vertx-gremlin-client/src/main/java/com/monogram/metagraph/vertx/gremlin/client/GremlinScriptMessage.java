3e0b2fce9b21e4f318c4ac36887981e861182659
weston <weston_contribute@163.com>
2016-05-23 12:33:22 +0800

    add Gremlin client

--- vertx-gremlin-client/src/main/java/com/monogram/metagraph/vertx/gremlin/client/GremlinScriptMessage.java
+++ vertx-gremlin-client/src/main/java/com/monogram/metagraph/vertx/gremlin/client/GremlinScriptMessage.java
@@ -3,12 +3,25 @@ package com.monogram.metagraph.vertx.gremlin.client;
 import java.util.Map;
 
 /**
+ * the message object submitted by client.
+ *
  * Created by zhaoliang(weston_contribute@163.com) on 2016/5/21.
  */
 public class GremlinScriptMessage {
 
+    /**
+     * gremlin language script.
+     */
     private String gremlinScript;
+
+    /**
+     * some parameters binding in gremlin script.
+     */
     private Map<String, String> parambindings;
+
+    /**
+     * the id of graph in which gremlin script executed.
+     */
     private String graphId;
 
     public String getGremlinScript() {
--- vertx-gremlin-client/src/main/java/com/monogram/metagraph/vertx/gremlin/client/VertexGremlinClient.java
+++ vertx-gremlin-client/src/main/java/com/monogram/metagraph/vertx/gremlin/client/VertexGremlinClient.java
@@ -1,31 +1,65 @@
 package com.monogram.metagraph.vertx.gremlin.client;
 
-import io.vertx.codegen.annotations.VertxGen;
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.net.NetClient;
 import io.vertx.core.net.NetServer;
+import io.vertx.core.net.NetSocket;
 
 /**
  * Created by zhaoliang(weston_contribute@163.com) on 2016/5/21.
  */
-@VertxGen
-public class VertexGremlinClient {
+public abstract class VertexGremlinClient extends AbstractVerticle {
 
-    private NetServer server;
+    private NetClient client;
+    private String host;
+    private int port;
+    private NetSocket socket;
+
+    public VertexGremlinClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public VertexGremlinClient() {
+        super();
+    }
+
+    @Override
+    public void start() throws Exception {
+        vertx.createNetClient().connect(port, host, res -> {
+
+            if (res.succeeded()) {
+                socket = res.result();
+                socket.handler(buffer -> {
+                    System.out.println("Net client receiving: " + buffer.toString("UTF-8"));
+                });
+
+                // Now send some data
+                for (int i = 0; i < 10; i++) {
+                    socket.write("test");
+                }
+            } else {
+                System.out.println("Failed to connect : " + res.cause());
+            }
+        });
+    }
+
 
     public NetServer connect(Vertx vertx, JsonObject config, String host, int port) {
         return vertx.createNetServer().listen(port, host);
     }
 
     public void close() {
-        server.close();
+        client.close();
     }
 
     public Object execute(GremlinScriptMessage message) {
         return null;
     }
 
-    public Object execute(String gremlinScriptMessageJson){
+    public Object execute(String gremlinScriptMessageJson) {
         return null;
     }
 
