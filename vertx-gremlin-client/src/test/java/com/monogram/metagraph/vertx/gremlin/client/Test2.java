cea378aa9b06d8a7fc42d40ec926f4c16bf9f0c3
weston <weston_contribute@163.com>
2016-05-24 15:52:46 +0800

    modify

--- vertx-gremlin-client/src/test/java/com/monogram/metagraph/vertx/gremlin/client/Test2.java
+++ vertx-gremlin-client/src/test/java/com/monogram/metagraph/vertx/gremlin/client/Test2.java
@@ -1,7 +1,37 @@
 package com.monogram.metagraph.vertx.gremlin.client;
 
+import java.util.function.Function;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.net.NetClient;
+import io.vertx.core.net.NetSocket;
+
 /**
  * Created by zhaoliang{weston_contribute@163.com} on 2016/5/24.
  */
 public class Test2 {
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        NetClient netClient = vertx.createNetClient();
+        netClient.connect(12345, "localhost", handler -> {
+            if (handler.succeeded()) {
+                System.out.println("connected!");
+                NetSocket socket = handler.result();
+                socket.handler(buffer -> {
+                    String result = buffer.toString();
+                    System.out.println(result);
+                    if (result.equals("hello")) {
+                        socket.write("world");
+                    } else if (result.equals("world")) {
+                        socket.write("hello");
+                    }
+                });
+                socket.write("hello");
+            } else {
+                handler.failed();
+            }
+        });
+        netClient.close();
+    }
+
 }
